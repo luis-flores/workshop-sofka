8edecdd0cd6d5336ddc1af7a28c5d86a7747ea28
Luis Diego Flores Villalobos <flores.luisdiego@gmail.com>
2023-11-27 21:20:57 -0600

    06_stream: Mejoras para no listar objetos sino nombres. Comparador con lambda.

--- 06_stream/src/test/java/com/example/demo/DojoStreamTest.java
+++ 06_stream/src/test/java/com/example/demo/DojoStreamTest.java
@@ -8,6 +8,20 @@ import java.util.stream.Collectors;
 
 public class DojoStreamTest {
 
+    private final Comparator<Player> comparadorMejorJugador = (Player p1, Player p2) -> {
+        if (p1.getWinners() > p2.getWinners())
+            return 1;
+        if (p1.getWinners() < p2.getWinners())
+            return -1;
+
+        if (p1.getGames() > p2.getGames())
+            return 1;
+        if (p1.getGames() < p2.getGames())
+            return -1;
+
+        return 0;
+    };
+
     @Test
     void converterData(){
         List<Player> list = CsvUtilFile.getPlayers();
@@ -17,34 +31,39 @@ public class DojoStreamTest {
     @Test
     void jugadoresMayoresA35(){
         List<Player> list = CsvUtilFile.getPlayers();
-        Set<Player> result = list.stream()
+        List<String> jugadoresMayoresA35 = list.stream()
             .filter(jugador -> jugador.getAge() > 35)
-            .collect(Collectors.toSet());
-        result.forEach(System.out::println);
+            .map(Player::getName)
+            .collect(Collectors.toList());
+
+        jugadoresMayoresA35.forEach(System.out::println);
     }
 
     @Test
     void jugadoresMayoresA35SegunClub(){
         List<Player> list = CsvUtilFile.getPlayers();
-        Map<String, List<Player>> result = list.stream()
+        Map<String, List<String>> result = list.stream()
             .filter(player -> player.getAge() > 35)
-            .distinct()
-            .collect(Collectors.groupingBy(Player::getClub));
+            .collect(Collectors.groupingBy(
+                Player::getClub,
+                Collectors.mapping(Player::getName, Collectors.toList())
+            ));
 
         result.forEach((key, jugadores) -> {
-            System.out.println("\n");
-            System.out.println(key + ": ");
+            System.out.println((!key.isEmpty() ? key : "Without club") + ": ");
             jugadores.forEach(System.out::println);
+            System.out.println();
         });
     }
 
     @Test
     void mejorJugadorConNacionalidadFrancia(){
         List<Player> list = CsvUtilFile.getPlayers();
-        Player mejorJugador = list.stream()
+        String mejorJugador = list.stream()
             .filter(player -> player.getNational().equals("France"))
-            .max(new BestPlayerComparator())
-            .get();
+            .max(comparadorMejorJugador)
+            .get()
+            .getName();
 
         System.out.println(mejorJugador);
     }
@@ -67,7 +86,7 @@ public class DojoStreamTest {
     void clubConElMejorJugador(){
         List<Player> list = CsvUtilFile.getPlayers();
         String club = list.stream()
-          .max(new BestPlayerComparator())
+          .max(comparadorMejorJugador)
           .get()
           .getClub();
 
@@ -77,9 +96,10 @@ public class DojoStreamTest {
     @Test
     void elMejorJugador(){
         List<Player> list = CsvUtilFile.getPlayers();
-        Player mejorJugador = list.stream()
-           .max(new BestPlayerComparator())
-           .get();
+        String mejorJugador = list.stream()
+           .max(comparadorMejorJugador)
+           .get()
+           .getName();
 
         System.out.println(mejorJugador);
     }
@@ -91,7 +111,7 @@ public class DojoStreamTest {
             .collect(Collectors.groupingBy(
                 Player::getNational,
                 Collectors.collectingAndThen(
-                    Collectors.maxBy(new BestPlayerComparator()),
+                    Collectors.maxBy(comparadorMejorJugador),
                     player -> player.map(p -> p.getName()).orElse(null)
                 )
             ));
