7209fbd6dc841553feb385c27a1d9c8c56dc3786
Luis Diego Flores Villalobos <flores.luisdiego@gmail.com>
2023-12-01 09:52:36 -0500

    05_mongo: Borrado codigo comentado
    Se eliminan imports y codigo comentado durante revision de pruebas

--- 05_transaction_mongo/src/test/java/com/workshop/transactionmongo/controller/TransactionControllerImplTests.java
+++ 05_transaction_mongo/src/test/java/com/workshop/transactionmongo/controller/TransactionControllerImplTests.java
@@ -1,31 +1,17 @@
 package com.workshop.transactionmongo.controller;
 
-import com.workshop.transactionmongo.documents.Account;
-import com.workshop.transactionmongo.documents.Client;
 import com.workshop.transactionmongo.dto.AccountDTO;
 import com.workshop.transactionmongo.dto.ClientDTO;
 import com.workshop.transactionmongo.dto.TransactionDTO;
-import com.workshop.transactionmongo.service.IClientService;
 import com.workshop.transactionmongo.service.ITransactionService;
-import com.workshop.transactionmongo.service.TransactionServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -33,26 +19,15 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.*;
 
-//@RunWith(SpringRunner.class)
-//@RestClientTest(TransactionControllerImpl.class)
 @ExtendWith(MockitoExtension.class)
 public class TransactionControllerImplTests {
 
-//    @InjectMocks
     private TransactionControllerImpl transactionController;
-//    @Mock
     private ITransactionService transactionService;
-
-//    private RestTemplate restTemplate;
-//    private MockRestServiceServer mockServer;
 
     @BeforeEach
     public void setUp() {
-//        restTemplate = new RestTemplate();
-//        mockServer = MockRestServiceServer.createServer(restTemplate);
         transactionService = Mockito.mock(ITransactionService.class);
         transactionController = new TransactionControllerImpl(transactionService);
     }
@@ -64,14 +39,10 @@ public class TransactionControllerImplTests {
         ClientDTO client = new ClientDTO("id", "client-name", "client-email", account);
         transactions.add(new TransactionDTO("id", "type", new BigDecimal(10), new BigDecimal(2), client));
         when(transactionService.findAll()).thenThrow(HttpServerErrorException.InternalServerError.class).thenReturn(transactions);
-//        mockServer.expect(requestTo("/transactions"))
-//            .andRespond(withSuccess("[]", MediaType.APPLICATION_JSON));
 
         ResponseEntity<List<TransactionDTO>> response = transactionController.findAll();
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        assertThat(response.getBody()).isEmpty();
-//        mockServer.verify();
         Mockito.verify(transactionService).findAll();
     }
 }
--- 05_transaction_mongo/src/test/java/com/workshop/transactionmongo/service/ClientServiceImplTests.java
+++ 05_transaction_mongo/src/test/java/com/workshop/transactionmongo/service/ClientServiceImplTests.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.math.BigDecimal;
@@ -20,7 +19,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-//@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class ClientServiceImplTests {
     @InjectMocks
